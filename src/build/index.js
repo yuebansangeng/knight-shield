0a78615ce032d93ba2a75ddb1d8f31599a6b179a
zhangyue <zygeilit@icloud.com>
2018-04-20 20:10:18 +0800

    new

--- src/build/index.js
+++ src/build/index.js
@@ -8,53 +8,72 @@ const { join } = require('path')
 // cpath 组件调用命令传入的路径
 let [ a, b, cpath ] = process.argv
 
+
 // 获取目录下文件夹的名字
-const getDemos =
+var getDemos =
   source =>
     readdirSync(source)
       .map(name => join(source, name))
       .filter(source => lstatSync(source).isDirectory())
-      .map(name => name.split('\/')[name.split('\/').length - 1])
-      .map(name => ({ 'name': name }))
+      .map(name => {
+        return {
+          'name': name.split('\/')[name.split('\/').length - 1],
+          'hasEditableProps': !!fs.existsSync(path.join(name, 'editable-props.js'))
+        }
+      })
 
-ejs.renderFile(
-  path.join(__dirname, 'stories.ejs'),
-  {
-    'demos': getDemos(path.join(cpath, 'demos'))
-  },
-  {}, // ejs options
-  (err, storiesjs) => {
-    if (err) throw err
+console.log(JSON.stringify(getDemos(path.join(cpath, 'demos'))))
 
-    // 创建config文件
-    ejs.renderFile(
+var readStoriesjs = (callback) => {
+  ejs.renderFile(
+    path.join(__dirname, 'stories.ejs'),
+    {
+      'demos': getDemos(path.join(cpath, 'demos'))
+    },
+    {}, // ejs options
+    (err, storiesjs) => {
+      if (err) throw err
+      callback(storiesjs)
+    }
+  )
+}
+
+
+const readConfigjs = (callback) => {
+  ejs.renderFile(
     path.join(__dirname, 'config.ejs'),
     {
       'cmpRootPath': cpath
     },
     {}, // ejs options
-    (err, str) => {
+    (err, configjs) => {
+      if (err) throw err
+      callback(configjs)
+    }
+  )
+}
+
+
+readStoriesjs(storiesjs => {
+  readConfigjs(configjs => {
+
+    // 创建config文件
+    fs.writeFile(path.join(__dirname, '..', 'config.js'), configjs, (err) => {
       if (err) throw err
 
-      // 创建config文件
-      fs.writeFile(path.join(__dirname, '..', 'config.js'), str, (err) => {
+      console.log('the config file is saved!')
+
+      const buildFolderPath = path.join(cpath, '.build')
+      if (!fs.existsSync(buildFolderPath)) {
+        fs.mkdirSync(buildFolderPath)
+      }
+
+      // 在组建项目中创建配置文件
+      fs.writeFile(path.join(cpath, '.build', '.stories.js'), storiesjs, (err) => {
         if (err) throw err
 
-        console.log('the config file is saved!')
-
-        // 如何组件内部没有.build文件夹则创建一个
-        var cbpath = path.join(cpath, '.build')
-        if (!fs.existsSync(cbpath)) {
-          fs.mkdirSync(cbpath)
-        }
-
-        // 在组建项目中创建配置文件
-        fs.writeFile(path.join(cpath, '.build', '.stories.js'), storiesjs, (err) => {
-          if (err) throw err
-
-          console.log('the entry file is saved!')
-        })
+        console.log('the stories file is saved!')
       })
     })
-  }
-)
+  })
+})
