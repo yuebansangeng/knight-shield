103b43b58f779dbfa75b68278bdf5b44f0872552
zhangyue <zygeilit@icloud.com>
2018-04-30 02:04:45 +0800

    new

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,4 +1,6 @@
 
+const { spawn } = require('child_process')
+const colors = require('colors')
 const shelljs = require('shelljs')
 
 // cpath 组件调用命令传入的路径
@@ -6,8 +8,19 @@ let [ a, b, cpath ] = process.argv
 
 // 判断组件内部是否配置storybooks配置
 // 配置了则不适用内部的 storybook-lib config
-if (fs.existsSync(path.join(cpath, '.storybook'))) {
-  shelljs.exec(`start-storybook -s . -p 9001 -c ${cpath}/.storybook`)
-} else {
-  shelljs.exec(`start-storybook -s . -p 9001 -c ../.config.js`)
+// if (fs.existsSync(path.join(cpath, '.storybook'))) {
+//   shelljs.exec(`start-storybook -s . -p 9001 -c ${cpath}/.storybook`)
+// } else {
+//   shelljs.exec(`start-storybook -s . -p 9001 -c ../.config.js`)
+// }
+
+print(spawn('node', 'make-demos.js', cpath))
+print(spawn('gulp', { 'cwd': cpath }))
+print(spawn('npm', ['run', 'storybook'], { 'cwd': cpath }))
+print(spawn('gulp', ['watch'], { 'cwd': cpath }))
+
+
+function print (chilprocess) {
+  chilprocess.stdout.on('data', data => console.log(`${data}`.green))
+  chilprocess.stderr.on('data', data => console.log(`${data}`.red))
 }
