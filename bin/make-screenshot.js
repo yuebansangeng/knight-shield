b0a6ad2f7d8fc9c9fdb0d23be00428bd09043590
zhangyue <zygeilit@icloud.com>
2018-05-05 10:23:46 +0800

    new

--- bin/make-screenshot.js
+++ bin/make-screenshot.js
@@ -5,8 +5,16 @@ const path = require('path')
 
 let [ a, b, cpath ] = process.argv
 
-// let chilprocess = spawn('storybook-chrome-screenshot', [ '-p', '9001', '-c', '../src', '-o', `${cpath}/.build/.screenshot` ])
-// chilprocess.stdout.on('data', data => console.log(`${data}`.green))
-// chilprocess.stderr.on('data', data => console.log(`${data}`.yellow))
+function print (chilprocess) {
+  chilprocess.stdout.on('data', data => console.log(`${data}`.green))
+  chilprocess.stderr.on('data', data => console.log(`${data}`.yellow))
+}
+
+print(spawn('node', ['make-stories.js', cpath], { 'cwd': __dirname }))
+print(spawn('node', ['make-demos.js', cpath], { 'cwd': __dirname }))
+
+print(spawn('gulp', [], { 'cwd': cpath }))
+
+print(spawn('node', ['make-config.js', cpath], { 'cwd': __dirname }))
 
 shelljs.exec(`storybook-chrome-screenshot -p 9001 -c ${path.join(__dirname, '..', 'src')} -o ${cpath}/.build/.screenshot`)
