f18e31363cd8bbcda9d453ab5f1a5c178a4e09e7
zhangyue <zygeilit@icloud.com>
2018-05-26 16:03:22 +0800

    new

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,4 @@
 
-const log = require('single-line-log').stdout
 const { spawn } = require('child_process')
 const colors = require('colors')
 const shelljs = require('shelljs')
@@ -9,16 +8,24 @@ const fs = require('fs')
 // cpath 组件调用命令传入的路径
 let [ a, b, cpath ] = process.argv
 
+function colorLog (data) {
+  let printStr = `${data}`
+  if (printStr.match(/Storybook started on/ig)) {
+    printStr = `${printStr}`.green
+  }
+  process.stdout.write(printStr)
+} 
+
 function print (chilprocess) {
-  chilprocess.stdout.on('data', data => log(`${data}`.green))
-  chilprocess.stderr.on('data', data => log(`${data}`.yellow))
+  chilprocess.stdout.on('data', data => colorLog(data))
+  chilprocess.stderr.on('data', err_data => colorLog(err_data))
 }
 
-print(spawn('node', ['make-stories.js', cpath], { 'cwd': __dirname }))
-print(spawn('node', ['make-demos.js', cpath], { 'cwd': __dirname }))
+print(spawn('node', ['make-stories.js', cpath, '--colors'], { 'cwd': __dirname }))
+print(spawn('node', ['make-demos.js', cpath, '--colors'], { 'cwd': __dirname }))
 
 // 生成 lib 目录，以及内部转义好的文件
-print(spawn('gulp', [], { 'cwd': cpath }))
+print(spawn('gulp', ['--colors'], { 'cwd': cpath }))
 
 // 判断组件内部是否配置storybooks配置
 // 配置了则不适用内部的 storybook-lib config
@@ -29,12 +36,12 @@ if (fs.existsSync(cpathStbkFlod)) {
   print(spawn('start-storybook', stbPreArgs.concat([ cpathStbkFlod ]), { 'cwd': cpath }))
 } else {
   // 生成 config 文件
-  print(spawn('node', ['make-config.js', cpath], { 'cwd': __dirname }))
+  print(spawn('node', ['make-config.js', cpath, '--colors'], { 'cwd': __dirname }))
   print(spawn('start-storybook', stbPreArgs.concat([ path.join(__dirname, '..', 'src') ]), { 'cwd': cpath }))
 }
 
 // 监控文件目录变化
-print(spawn('gulp', ['watch'], { 'cwd': cpath })) 
+print(spawn('gulp', ['watch', '--colors'], { 'cwd': cpath })) 
 
 // 打开本地调试浏览器
 spawn('/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome', ['--enable-speech-input', 'http://localhost:9001'])
