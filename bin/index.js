72f774434d335daa15d47a8bd9275ba02337b287
zhangyue <zygeilit@icloud.com>
2018-06-08 09:11:01 +0800

    revmoe gulp

--- bin/index.js
+++ bin/index.js
@@ -55,40 +55,9 @@ let main = async () => {
     )
   })
 
-  // 生成 lib 目录，以及内部转义好的文件
-  await new Promise((resolve, reject) => {
-    // 执行 gulp 命令
-    let cp_n = spawn('node', [
-      'node_modules/gulp/bin/gulp.js',
-      // 调整 gulpfile 配置文件的获取路径
-      '--gulpfile', path.join(__dirname, '..', 'lib', 'gulpfile.js'),
-      // 重定向 gulp 命令执行的路径到组件项目根目录
-      '--cwd', cpath,
-      '--colors'
-    ], { 'cwd': cpath })
-    // 监听返回值，close时结束
-    cp_n.stdout.on('data', data => colorLog(data))
-    cp_n.stderr.on('data', err_data => colorLog(err_data))
-    cp_n.stderr.on('close', () => {
-      resolve(true)
-    })
-  })
-
   // 运行 storyrbooks 调试环境
   // 使用 spwan 执行，需要和 gulp watch 命令并行执行
   print(spawn('start-storybook', [ '-s', '.', '-p', '9001', '-c', path.join(__dirname, '..', 'lib') ], { 'cwd': cpath }))
-
-  // 监控文件目录变化
-  // 使用 spwan 执行，需要和 start-storybook 命令并行执行
-  print(spawn('node', [
-    'node_modules/gulp/bin/gulp.js',
-    // 调整 gulpfile 配置文件的获取路径
-    '--gulpfile', path.join(__dirname, '..', 'lib', 'gulpfile.js'),
-    // 重定向 gulp 命令执行的路径到组件项目根目录
-    '--cwd', cpath,
-    'watch',
-    '--colors'
-    ], { 'cwd': cpath }))
 
   // 打开本地调试浏览器
   spawn('/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome', ['--enable-speech-input', 'http://localhost:9001'])
