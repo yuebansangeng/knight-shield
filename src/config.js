88940fe733cfd307b00bdc3712c84b75dea0602a
zhangyue <zygeilit@icloud.com>
2018-06-12 17:27:17 +0800

    new add react

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 
+import React from 'react'
 import { storiesOf, configure } from '@storybook/react'
 import { withDocs, withReadme }  from 'storybook-readme'
 import { withKnobs } from '@storybook/addon-knobs/react'
-import backgrounds from '@storybook/addon-backgrounds'
 
 import { stories, name, readme } from './stories.js'
 import handleType from './util'
@@ -11,11 +11,7 @@ configure(
   () => {
     let storiesInstence = storiesOf(name, module)
     storiesInstence.addDecorator(withKnobs);
-    storiesInstence.addDecorator(withReadme([readme]))
-    storiesInstence.addDecorator(backgrounds([
-      { 'name': "twitter", 'value': "white", 'default': true },
-      { 'name': "facebook", 'value': "#3b5998" }
-    ]))
+    storiesInstence.addDecorator(withReadme([ readme ]))
 
     stories.forEach(({ name, story }) => {
       const { content: Component, editableProps, doc = '' } = story
