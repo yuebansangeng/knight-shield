063ccb8554df46a357af0fa3804bef9aeebee19a
zhangyue <zygeilit@icloud.com>
2018-06-13 18:50:17 +0800

    remove doc

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import { storiesOf, configure } from '@storybook/react'
-import { withDocs, withReadme }  from 'storybook-readme'
+import { withReadme }  from 'storybook-readme' // withDocs
 import { withKnobs } from '@storybook/addon-knobs/react'
 
 import { stories, name, readme } from './stories.js'
@@ -15,10 +15,10 @@ configure(
 
     stories.forEach(({ name, story }) => {
       const { content: Component, editableProps, doc = '' } = story
-      storiesInstence.add(name, withDocs(doc, () => {
+      storiesInstence.add(name, () => {
         const params = handleType(editableProps)
         return <Component {...params} />
-      }))
+      })
     })
   },
   module
