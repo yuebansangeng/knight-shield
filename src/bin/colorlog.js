5af6b158b2610e4f5a7db974a598ef7900a66daa
zhangyue <zygeilit@icloud.com>
2018-07-11 16:36:07 +0800

    new

---
+++ src/bin/colorlog.js
@@ -0,0 +1,8 @@
+
+module.exports = (data) => {
+  let printStr = `${data}`
+  if (printStr.match(/Storybook started on/ig)) {
+    printStr = `${printStr}`.green
+  }
+  process.stdout.write(printStr)
+}
