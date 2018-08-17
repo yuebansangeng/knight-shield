70b437b331e226a1f7490ca1eb1c00dc8a163bd3
zhangyue <zygeilit@icloud.com>
2018-08-17 20:20:35 +0800

    new

--- src/bin/build-statics.js
+++ src/bin/build-statics.js
@@ -1,75 +1,36 @@
 
 import path from 'path'
-import Promise from 'bluebird'
-import { spawn } from 'child_process'
 import minimist from 'minimist'
-import makeStories from '../make-stories'
-import generateHttpHAREntry from '../generate-http-har-entry'
-import readrc from '@beisen/read-rc'
 import dotenv from 'dotenv'
-import fg from 'fast-glob'
+import buildCmpStatics from '../build-cmp-statics'
 
 dotenv.config({ 'path': path.join(__dirname, '..', '..', '.env') })
 
 // 统一添加前缀组件模块前缀
-const main = async () => {
-  const argv = minimist(process.argv.slice(2))
+const main = async (o) => {
+  const {
+    independent,
+    'source-path': sourcePath
+  } = minimist(process.argv.slice(2))
 
   // 开发者可以自定义构建静路径
   let cpath = process.cwd()
-  if (argv['source-path']) {
-    cpath = path.join(process.cwd(), argv['source-path'])
+  if (sourcePath) {
+    cpath = path.join(process.cwd(), sourcePath)
   }
 
-  const { 'name': module, version } = require(`${cpath}/package.json`)
+  let resp = null
+  if (independent) {
 
-  // 获取rc配置文件中的配置
-  let rc = readrc(cpath)
-  // 组件名称
-  const cname = rc.name || module
-
-  // 生成 stories.js 配置文件
-  let components = [cpath]
-  if (rc.components) {
-    components = await fg.sync(rc.components, { 'onlyDirectories': true })
-    components = components.map(p => path.join(cpath, p))
-  }
-  makeStories({ components })
-  console.log(`配置文件( stories.js )生成完毕`)
-
-  // 生成 https HAR 配置文件
-  generateHttpHAREntry({
-    // argv['http-har-path']:
-    // 该构建任务是jenkins调用，无法在执行指令时配置参数，只能在rc文件中获取
-    'httpHARPath': rc.mock.https,
-    cpath
-  })
-  console.log(`配置文件( https.json )生成完毕`)
-  console.log(`开始编译静态资源：${cname}/${version}`)
-
-  // 构建
-  let { code, message } = await new Promise((resolve, reject) => {
-    let resmsg = []
-    let build_cp = spawn('node',
-      [
-        'node_modules/.bin/build-storybook',
-        '-c', path.join(__dirname, '..', '.storybook'),
-        '-o', `${cpath}/storybook-static/${cname}/${version}`
-      ],
-      { }
-    )
-    build_cp.stdout.on('data', data => resmsg.push(`${data}`))
-    build_cp.stderr.on('data', data => resmsg.push(`${data}`))
-    build_cp.on('close', code => {
-      // 如果不join的方式输出log，会在输出信息换行时出现问题
-      resolve({ code, 'message': resmsg.join('') })
-    })
-  })
-
-  if (code !== 0) {
-    throw new Error(message)
   } else {
-    console.log(message)
+    resp = await buildCmpStatics({ cpath })  
+  }
+
+  if (resp.code !== 0) {
+    throw new Error(resp.message)
+  } else {
+    res = true
+    console.log(resp.message)
   }
 }
 
---
+++ src/build-cmp-statics.js
@@ -0,0 +1,60 @@
+
+import path from 'path'
+import Promise from 'bluebird'
+import { spawn } from 'child_process'
+import minimist from 'minimist'
+import makeStories from '../make-stories'
+import generateHttpHAREntry from '../generate-http-har-entry'
+import readrc from '@beisen/read-rc'
+import dotenv from 'dotenv'
+import fg from 'fast-glob'
+
+dotenv.config({ 'path': path.join(__dirname, '..', '..', '.env') })
+
+// 统一添加前缀组件模块前缀
+export default async (o) => {
+  const { cpath } = o
+  const { 'name': module, version } = require(`${cpath}/package.json`)
+
+  // 获取rc配置文件中的配置
+  let rc = readrc(cpath)
+
+  // 组件名称
+  const cname = rc.name || module
+
+  // 生成 stories.js 配置文件
+  let components = [cpath]
+  if (rc.components) {
+    components = await fg.sync(rc.components, { 'onlyDirectories': true })
+    components = components.map(p => path.join(cpath, p))
+  }
+  makeStories({ components })
+
+  // 生成 https HAR 配置文件
+  generateHttpHAREntry({
+    // 该构建任务是jenkins调用，无法在执行指令时配置参数，只能在rc文件中获取
+    'httpHARPath': rc.mock.https,
+    cpath
+  })
+
+  console.log(`开始编译静态资源：${cname}/${version}`)
+
+  // 构建
+  return await new Promise((resolve, reject) => {
+    let resmsg = []
+    let build_cp = spawn('node',
+      [
+        'node_modules/.bin/build-storybook',
+        '-c', path.join(__dirname, '..', '.storybook'),
+        '-o', `${cpath}/storybook-static/${cname}/${version}`
+      ],
+      { }
+    )
+    build_cp.stdout.on('data', data => resmsg.push(`${data}`))
+    build_cp.stderr.on('data', data => resmsg.push(`${data}`))
+    build_cp.on('close', code => {
+      // 如果不join的方式输出log，会在输出信息换行时出现问题
+      resolve({ code, 'message': resmsg.join('') })
+    })
+  })
+}
