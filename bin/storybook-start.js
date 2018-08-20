6d7575837dc77f9b93c2e6ea8eca52c5f2145ee1
zhangyue <zygeilit@icloud.com>
2018-08-20 12:47:01 +0800

    向前兼容

---
+++ bin/storybook-start.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+// 向前兼容，不推荐使用该方式
+// 推荐的使用方式 sbl storybook start --prot [?] --source [?]
+
+const yeomanEnv = require('yeoman-environment')
+const argv = require('minimist')(process.argv.slice(2))
+
+const env = yeomanEnv.createEnv()
+  .register(require.resolve('../lib/commands/storybook'), 'storybook')
+
+const { source, port } = argv
+
+env.run(`storybook start`, { source, port })
