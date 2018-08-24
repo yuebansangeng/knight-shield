ca141ffbf4d171eb32d6659a9c63d829df2c2bd7
zhangyue <zygeilit@icloud.com>
2018-08-24 22:55:16 +0800

    validate

--- src/commands/publish/component/check.js
+++ src/commands/publish/component/check.js
@@ -3,19 +3,13 @@ import request from 'request-promise'
 
 export default async (o) => {
   const { CMP_SERVER_HOST } = process.env
-  const { 'rc': { name, team, category }, 'package': { 'name': module } } = o
+  const { 'rc': { name, team }, 'package': { 'name': module } } = o
 
   if (!name) {
     throw new Error(`请在 rc 配置文件中，配置 name 字段`)
   }
-  if (!name.match(/^[A-Za-z\-\d]+?$/)) {
-    throw new Error(`rc 文件中，name 字段只能包含有是字母、数字、中划线`)
-  }
-  if (!team) {
-    throw new Error(`请在 rc 文件中，配置 team 字段（team 字段将会用来组件唯一性验证，以及搜索功能）`)
-  }
-  if (!category) {
-    
+  if (!name.match(/^[A-Za-z\-\d@\/]+?$/)) {
+    throw new Error(`rc 文件中，name 字段只能包含有[A-Z,a-z,-,0-9,@,/]`)
   }
 
   const { code, message, data } =
