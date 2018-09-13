864512ce0ae84ead710cb6881fc5a0270f4c9a7d
liumingyuan <liumingyuan@beisen.com>
2018-09-13 17:36:49 +0800

    dsz

--- src/commands/publish/index.js
+++ src/commands/publish/index.js
@@ -2,8 +2,7 @@
 import path from 'path'
 import Generator from 'yeoman-generator'
 import readrc from '../../helpers/read-rc'
-import { spawnSync } from 'child_process'
-import fs from 'fs'
+
 
 export default class extends Generator {
 
@@ -19,17 +18,7 @@ export default class extends Generator {
   _private_resolve (compoesePath) {
     let packinfo = require(`${this.contextRoot}/package.json`)
     let contextRoot = this.contextRoot
-    //修改rc文件
-    const { stdout } = spawnSync('git', ['config', 'user.name'])
-    let username = `${stdout}`.replace(/^\s+|\s+$/, '')
 
-    let rcinfo =  JSON.parse(fs.readFileSync(`${this.contextRoot}/.bscpmrc`,'utf-8'));
-
-    let developers = Array.isArray(rcinfo.developers) ? rcinfo.developers : []
-
-    rcinfo.developers = developers.includes(`${username}`) ? developers  : developers.concat(`${username}`)
-
-    fs.writeFileSync(`${this.contextRoot}/.bscpmrc`, JSON.stringify(rcinfo,null,2),'utf-8'); 
 
     let { source } = this.options
     if (source) {
